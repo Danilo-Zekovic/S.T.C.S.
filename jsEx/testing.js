8eea0061c90ee0c7ae2f88898c190141fdc53a9f
Danilo Zekovic <danilozeka93@gmail.com>
2016-03-03 14:36:45 -0500

    testing modules

--- jsEx/testing.js
+++ jsEx/testing.js
@@ -1,8 +1,8 @@
-var foo2 = [0,1];
+/*var foo2 = [0,1];
 
 /*for (i=0; i< 10; i++){
   foo.push(Math.floor(Math.random() * 10) + 1);
-}*/
+}
 
 function Fack(num){
   if (num == 1){
@@ -24,3 +24,17 @@ console.log(foo2);
 for (bar in foo2){
   console.log(foo2[bar]);
 }
+*/
+var prompt = require('prompt');
+var numAdd = require('./ex.js').addNum;
+
+
+
+console.log(numAdd.add(1,2));
+
+/*prompt.start();
+
+var bro = prompt.get(['bro1'], function(err,result){
+  console.log(result.bro1);
+});
+console.log(bro);*/
