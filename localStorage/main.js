d99d923a98b590faa5485ec212a0ba7c0a4ae5c1
Danilo Zekovic <danilozeka93@gmail.com>
2016-04-20 01:41:55 -0400

    created js file for logic

---
+++ localStorage/main.js
@@ -0,0 +1,12 @@
+function myFunction(){
+  if(typeof(Storage) !== "undefined") {
+      console.log("There is local storage...");
+      localStorage.setItem("message", "It works!");
+  } else {
+      console.log("There is no local storage!!!");
+  }
+}
+
+function display(){
+  document.getElementById("storage-content").innerHTML = localStorage.getItem("message");
+}
